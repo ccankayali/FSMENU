1a64e6ac7cd18b063a13f63abd1047ff65f9b20c
Can Kayalı <volkand135@gmail.com>
2023-12-18 21:43:07 +0300

    Login Design

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 
 const EntryScreen = () => {
   const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
+  const [verificationCode, setVerificationCode] = useState('');
+  const [isVerified, setIsVerified] = useState(false);
 
+
+  // Random number generator
   function generateRandomNumber() {
     return Math.floor(Math.random() * 1000);
   }
@@ -11,25 +15,41 @@ const EntryScreen = () => {
     setRandomNumber(generateRandomNumber());
   }
 
+  // Verified Control
+  function handleVerification() {
+    if (verificationCode === randomNumber) {
+      setIsVerified(true);
+    } else {
+      setIsVerified(false);
+    }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
       <div className="max-w-screen-lg p-8 flex flex-col items-center">
         <div className="text-4xl mb-3 text-white">{randomNumber}</div>
         <div className="w-full max-w-xs">
-            <h1 className="text-white flex items-center justify-center p-2">Lütfen Doğrulama Kodunu Giriniz</h1>
+          <h1 className="text-white flex items-center justify-center p-2">Lütfen Doğrulama Kodunu Giriniz</h1>
           <input
             type="text"
             placeholder="Doğrulama Kodu"
+            value={verificationCode}
+            onChange={(e) => setVerificationCode(e.target.value)}
             className="w-full p-4 mb-8 border rounded"
           />
-          <div className="flex w-full justify-between ">
+          <div className="flex w-full justify-between">
             <button
               className="w-48 p-4 bg-blue-500 text-white rounded-xl"
               onClick={handleNewNumber}
             >
               Yeni Sayı
             </button>
-            <button className="w-48 p-4 bg-blue-500 text-white rounded-xl">İlerle</button>
+            <button
+              className="w-48 p-4 bg-green-500 text-white rounded-xl"
+              onClick={handleVerification}
+            >
+              İlerle
+            </button>
           </div>
         </div>
       </div>
