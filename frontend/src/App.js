038e75984c39793adc1cd2c8ed4da5ac9d79ac1f
Can Kayalı <volkand135@gmail.com>
2023-12-19 23:13:18 +0300

    Login Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './index.css';
 
 function App() {
   return (
-    <div className="bg-black">
+    <div>
       <Login />
     </div>
   );
--- frontend/src/components/Log.js
+++ frontend/src/components/Log.js
@@ -4,16 +4,22 @@ const EntryScreen = () => {
   const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
   const [verificationCode, setVerificationCode] = useState('');
   const [isVerified, setIsVerified] = useState(false);
+  const [userId, setUserId] = useState(null);
 
   // Random number generator
   function generateRandomNumber() {
     return Math.floor(Math.random() * 1000);
   }
 
-  // Handle verification process
+  // Yeni sayı oluşturma
+  function handleNewNumber() {
+    setRandomNumber(generateRandomNumber());
+  }
+
+  // Doğrulama işlemi
   async function handleVerification() {
     try {
-      const response = await fetch('http://localhost:3001/authenticate', {
+      const response = await fetch('http://localhost:3000/authenticate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -25,25 +31,48 @@ const EntryScreen = () => {
 
       if (data.success) {
         setIsVerified(true);
+        setUserId(data.userId);
       } else {
         setIsVerified(false);
+        setUserId(null);
       }
     } catch (error) {
       console.error('Auth API error:', error);
     }
   }
 
-  // Handle generating a new random number and reset verification status
-  function handleNewNumber() {
-    setRandomNumber(generateRandomNumber());
-    setIsVerified(false);
-    setVerificationCode('');
+  // Yeni sayı ve kod oluşturma
+  async function handleNewCodeAndNumber() {
+    try {
+      const response = await fetch('http://localhost:3000/generateCode', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setRandomNumber(data.code);
+        setIsVerified(false);
+        setVerificationCode('');
+      } else {
+        console.error('Code generation error:', data.message);
+      }
+    } catch (error) {
+      console.error('Code generation error:', error);
+    }
   }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
-      <button className="p-4 mt-5 bg-red-500 text-white rounded-xl mb-4">Admin</button>
-      <div className="max-w-screen-lg p-8 flex flex-col items-start">
+      <button
+          className="p-4 mt-5 bg-red-500  text-white rounded-xl mb-4">
+          Admin
+        </button>
+      <div className="max-w-screen-lg p-8 flex flex-col items-start">        
         {isVerified ? (
           <h1 className="text-4xl mb-3 text-white">Doğrulama Başarılı!</h1>
         ) : (
@@ -58,17 +87,17 @@ const EntryScreen = () => {
                 placeholder="Doğrulama Kodu"
                 value={verificationCode}
                 onChange={(e) => setVerificationCode(e.target.value)}
-                className="w-full p-4 mb-4 border rounded"
+                className="w-full p-4 mb-8 border rounded"
               />
-              <div className="flex w-full flex-col items-center">
+              <div className="flex w-full justify-center">
                 <button
-                  className="w-full p-4 bg-blue-500 text-white rounded-xl mb-2"
+                  className="w-48 p-4 bg-blue-500 text-white rounded-xl"
                   onClick={handleNewNumber}
                 >
                   Yeni Sayı
                 </button>
                 <button
-                  className="w-full p-4 bg-green-500 text-white rounded-xl"
+                  className="w-48 p-4 bg-green-500 text-white rounded-xl ml-4"
                   onClick={handleVerification}
                 >
                   İlerle
@@ -82,4 +111,4 @@ const EntryScreen = () => {
   );
 };
 
-export default EntryScreen;
+export default EntryScreen; 
